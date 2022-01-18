e08d52720612b9b391e439a3de0ec45a8bdc5c75
nishatvpn <92996099+nishatvpn@users.noreply.github.com>
2022-01-18 22:05:43 +0800

    Update xx_openconnect.sh

--- xx_openconnect.sh
+++ xx_openconnect.sh
@@ -2,10 +2,10 @@
 cp /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 
 #Database Details
-db_host='209.159.152.66';
-db_user='crypticv_fplusvpnapp';
-db_pass='@@F1r3n3t@@';
-db_name='crypticv_fplusvpnapp';
+db_host='82.223.165.66';
+db_user='nishatvip_ahadtech';
+db_pass='@@@@F1r3n3t';
+db_name='nishatvip_ahadtech';
 
 install_require()
 {
