76ab47bcca3ab557b4663cb0b6a4310213103124
nishatvpn <92996099+nishatvpn@users.noreply.github.com>
2022-01-18 22:06:58 +0800

    Update xx_ovpnws.sh

--- xx_ovpnws.sh
+++ xx_ovpnws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='209.159.152.66';
-USER='crypticv_fplusvpnapp';
-PASS='@@F1r3n3t@@';
-DBNAME='crypticv_fplusvpnapp';
+HOST='82.223.165.66';
+USER='nishatvip_ahadtech';
+PASS='@@@@F1r3n3t';
+DBNAME='nishatvip_ahadtech';
 
 install_require()
 {
